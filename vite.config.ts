25e7637d6810de037466348a5667b60be8c3bc43
gytama-edu <gytama.edu2@gmail.com>
2026-07-05 11:12:26 +0700

    Add files via upload
    Signed-off-by: gytama-edu <gytama.edu2@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: process.env.VITE_BASE_PATH || './',
+    base: process.env.VITE_BASE_PATH || '/mission-control/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
